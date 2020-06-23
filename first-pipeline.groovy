3d4ed581c5104248b92053e803fca2bc3af76717
mrsk1225 <mrsk1225@gmail.com>
2020-06-23 00:21:01 -0700

    Update first-pipeline.groovy

--- first-pipeline.groovy
+++ first-pipeline.groovy
@@ -2,11 +2,40 @@ pipeline {
     agent any
     
     stages {
+        
         stage ('Checkout') {
             steps {
-                step ('Step 1') {
-                    echo 'Checkout stage - Step 1'
-                }
+                echo 'Checkout stage'
+            }
+        }
+        
+        stage ('Build') {
+            steps {
+                echo 'Build stage'
+            }
+        }
+        
+        stage ('Deploy to DEV') {
+            steps {
+                echo 'Deploy to DEV stage'
+            }
+        }
+        
+        stage ('Deploy to TEST') {
+            steps {
+                echo 'Deploy to TEST stage'
+            }
+        }
+        
+        stage ('Run Test Cases') {
+            steps {
+                echo 'Testing stage'
+            }
+        }
+        
+        stage ('Deploy to PROD') {
+            steps {
+                echo 'Deploy to PROD stage'
             }
         }
     }
